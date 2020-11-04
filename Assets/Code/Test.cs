1ca45257807c5b4e0c7aa2fe30f600d6aa3f7698
Black Face <bf.blackface@gmail.com>
2020-11-04 23:39:31 +0700

    Update

---
+++ Assets/Code/Test.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public string AnimString;
+    private Animator Anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Anim = this.GetComponent<Animator>();
+        Anim.SetTrigger(AnimString);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
